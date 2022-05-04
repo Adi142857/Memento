15a3f28c8e1d45b74506ba22e944ccd30e652153
Adi142857 <aditya14iyer@gmail.com>
2022-05-04 15:01:27 +0530

    server modified

--- server/index.js
+++ server/index.js
@@ -8,11 +8,12 @@ import postRoutes from './routes/posts.js';
 const app= express();
 
 
-app.use('/posts',postRoutes);
+
 app.use(bodyParser.json({limit:"30mb", extended:true}));
 //to send requests
 app.use(bodyParser.urlencoded({limit:"30mb", extended:true}));
 app.use(cors());
+app.use('/posts',postRoutes);
 const CONNECTION_URL='mongodb+srv://Aditya:aditya123@cluster0.aqdut.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
 
