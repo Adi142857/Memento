e15bf292dad0722c18493539a4ef2bbda7b42898
Adi142857 <aditya14iyer@gmail.com>
2022-06-04 14:09:08 +0530

    mobile friendly

--- client/src/App.js
+++ client/src/App.js
@@ -20,12 +20,12 @@ const App = () => {
   return (
     <Container maxWidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">Reminiscence</Typography>
+        <Typography className={classes.heading} variant="h2" align="center">Memento</Typography>
         <img className={classes.image} src={memories} alt="icon" height="60" />
       </AppBar>
       <Grow in>
         <Container>
-          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+          <Grid className={classes.mainContainer} container justify="space-between" alignItems="stretch" spacing={3}>
             <Grid item xs={12} sm={7}>
               <Posts setCurrentId={setCurrentId} />
             </Grid>
--- client/src/styles.js
+++ client/src/styles.js
@@ -1,6 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-export default makeStyles(() => ({
+export default makeStyles((theme) => ({
   appBar: {
     borderRadius: 15,
     margin: '30px 0',
@@ -15,4 +15,9 @@ export default makeStyles(() => ({
   image: {
     marginLeft: '15px',
   },
+  [theme.breakpoints.down('sm')]:{
+    mainContainer:{
+      flexDirection:"column-reverse"
+    }
+  }
 }));
