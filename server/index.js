75cbddbf9e5d994e18673a6e041bbd70c291eb76
Adi142857 <aditya14iyer@gmail.com>
2022-06-04 14:16:07 +0530

    env added

--- server/index.js
+++ server/index.js
@@ -3,10 +3,10 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import postRoutes from './routes/posts.js';
-
+import dotenv from 'dotenv';
 
 const app= express();
-
+dotenv.config();
 
 
 app.use(bodyParser.json({limit:"30mb", extended:true}));
@@ -14,10 +14,10 @@ app.use(bodyParser.json({limit:"30mb", extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb", extended:true}));
 app.use(cors());
 app.use('/posts',postRoutes);
-const CONNECTION_URL='mongodb+srv://Aditya:aditya123@cluster0.aqdut.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL,{useNewUrlParser:true, useUnifiedTopology:true})
+mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser:true, useUnifiedTopology:true})
       .then(()=>app.listen(PORT,()=>console.log(`Server running :${PORT}`)))
       .catch((error)=>console.log(error.message));
 
