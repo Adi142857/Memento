249cad0e1fb888c33ad34ae6eca6e230240bc86a
Adi142857 <aditya14iyer@gmail.com>
2022-06-04 11:12:29 +0530

    editing posts,likes,delete'

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,4 +2,4 @@ import { combineReducers } from 'redux';
 
 import posts from './posts';
 
-export default combineReducers({ posts });
+export const reducers = combineReducers({ posts });
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,11 +1,14 @@
-import  express  from "express";
+import express from 'express';
 
-const router=express.Router();
+import { getPosts, getPost, createPost, updatePost, likePost, deletePost } from '../controllers/posts.js';
 
-import {getPosts,createPost} from '../controllers/posts.js'
+const router = express.Router();
 
-
-router.get('/',getPosts);
-router.post('/',createPost);
+router.get('/', getPosts);
+router.post('/', createPost);
+router.get('/:id', getPost);
+router.patch('/:id', updatePost);
+router.delete('/:id', deletePost);
+router.patch('/:id/likePost', likePost);
 
 export default router;
