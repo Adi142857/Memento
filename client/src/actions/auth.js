20b2e1a191c9bf00d563ba9ebd9401916df932a2
Adi142857 <aditya14iyer@gmail.com>
2022-06-28 17:46:32 +0530

    redirect uri added

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,28 +1,26 @@
 import { AUTH } from '../constants/actionTypes';
 import * as api from '../api/index.js';
-import { useNavigate } from 'react-router-dom';
 
-export const signin=(formData,navigate)=> async(dispatch)=>{
+export const signin = (formData, router) => async (dispatch) => {
+  try {
+    const { data } = await api.signIn(formData);
 
-    try{
+    dispatch({ type: AUTH, data });
 
-        //log in
-        const {data}= await api.signIn(formData);
-        dispatch({type:AUTH, payload: data.result});
-        navigate('/')
-    }
-    catch(error){
-        console.log(error)
-    }
-}
-export const signup=(formData, navigate)=> async(dispatch)=>{
-    try{
-        //sign
-        const {data}= await api.signUp(formData);
-        dispatch({type:AUTH},data);
-        navigate('/')
-    }
-    catch(error){
-        console.log(error)
-    }
-}
+    router.push('/');
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const signup = (formData, router) => async (dispatch) => {
+  try {
+    const { data } = await api.signUp(formData);
+
+    dispatch({ type: AUTH, data });
+
+    router.push('/');
+  } catch (error) {
+    console.log(error);
+  }
+};
