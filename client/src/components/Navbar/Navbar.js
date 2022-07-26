64a33d898709f1ac4f87691fd8c130fb7d60d4ba
Adi142857 <aditya14iyer@gmail.com>
2022-07-26 23:08:33 +0530

    navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,53 +1,60 @@
-import React,{useState,useEffect} from "react";
-import { AppBar,Typography,Avatar,Toolbar, Button} from "@material-ui/core";
-import memories from '../../images/memories.png';
-import useStyles from './styles';
-import {Link,useLocation} from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { AppBar, Typography, Toolbar, Avatar, Button } from '@material-ui/core';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import {useNavigate} from 'react-router-dom'
+import decode from 'jwt-decode';
 
+import memoriesLogo from '../../images/memoriesLogo.png';
+import memoriesText from '../../images/memento.png';
+import * as actionType from '../../constants/actionTypes';
+import useStyles from './styles';
 
-const Navbar=()=>{
-  const classes= useStyles();
-  const dispatch=useDispatch(); 
-  const history=useNavigate();
-  const location=useLocation();
-  
-  const [user,setUser]=useState(JSON.parse(localStorage.getItem('profile')));
-  console.log("navbar",user);
+const Navbar = () => {
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const history = useHistory();
+  const classes = useStyles();
 
-  const logout=()=>{
-    dispatch({type:'LOGOUT'});
-    history('/');
+  const logout = () => {
+    dispatch({ type: actionType.LOGOUT });
+
+    history.push('/auth');
+
     setUser(null);
-  }
+  };
 
-  useEffect(()=>{
-    const token=user?.token;
+  useEffect(() => {
+    const token = user?.token;
+
+    if (token) {
+      const decodedToken = decode(token);
+
+      if (decodedToken.exp * 1000 < new Date().getTime()) logout();
+    }
 
     setUser(JSON.parse(localStorage.getItem('profile')));
-  },[location]);
-  
-  return(
-  <AppBar className={classes.appBar} position="static" color="inherit">
-    <div classsName={classes.brandContainer}>
-      <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">Memento</Typography>
-      <img className={classes.image} src={memories} alt="icon" height="60" />
-    </div>
-  <Toolbar className={classes.toolbar}>
-    {user ? (
-      <div className={classes.profile}>
-        <Avatar className={classes.purple} alt={user.name} src={user.imageUrl}>{user.name.charAt(0)}</Avatar>
-        <Typography className={classes.userName} variant="h6">{user.name}</Typography>
-        <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
-      </div>
-    ):(
-      <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
-    )}
-  </Toolbar>
-  </AppBar>
-  );
+  }, [location]);
 
+  return (
+    <AppBar className={classes.appBar} position="static" color="inherit">
+      <Link to="/" className={classes.brandContainer}>
+        <img component={Link} to="/" src={memoriesText} alt="icon" height="45px" />
+        <img className={classes.image} src={memoriesLogo} alt="icon" height="40px" />
+      </Link>
+      <Toolbar className={classes.toolbar}>
+        {user?.result ? (
+          <div className={classes.profile}>
+            <Avatar className={classes.purple} alt={user?.result.name} src={user?.result.imageUrl}>{user?.result.name.charAt(0)}</Avatar>
+            <Typography className={classes.userName} variant="h6">{user?.result.name}</Typography>
+            <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
+          </div>
+        ) : (
+          <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default Navbar;
--- client/src/components/Navbar/styles.js
+++ client/src/components/Navbar/styles.js
@@ -2,39 +2,54 @@ import { makeStyles } from '@material-ui/core/styles';
 import { deepPurple } from '@material-ui/core/colors';
 
 export default makeStyles((theme) => ({
-    appBar: {
-        borderRadius: 15,
-        margin: '30px 0',
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-      },
-      [theme.breakpoints.down('sm')]:{
-        mainContainer:{
-          flexDirection:"column-reverse"
-        }
-      },
+  appBar: {
+    borderRadius: 15,
+    margin: '30px 0',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '10px 50px',
+    [theme.breakpoints.down('sm')]: {
+      flexDirection: 'column',
+    },
+  },
   heading: {
-    color: 'rgba(0,183,255, 1)',
+    color: theme.palette.primary.main,
     textDecoration: 'none',
+    fontSize: '2em',
+    fontWeight: 300,
   },
   image: {
-    marginLeft: '15px',
+    marginLeft: '10px',
+    marginTop: '5px',
   },
   toolbar: {
     display: 'flex',
     justifyContent: 'flex-end',
     width: '400px',
+    [theme.breakpoints.down('sm')]: {
+      width: 'auto',
+    },
   },
   profile: {
     display: 'flex',
     justifyContent: 'space-between',
     width: '400px',
+    alignItems: 'center',
+    [theme.breakpoints.down('sm')]: {
+      width: 'auto',
+      marginTop: 20,
+      justifyContent: 'center',
+    },
+  },
+  logout: {
+    marginLeft: '20px',
   },
   userName: {
     display: 'flex',
     alignItems: 'center',
+    textAlign: 'center',
   },
   brandContainer: {
     display: 'flex',
