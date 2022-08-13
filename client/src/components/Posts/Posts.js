c64fcb68743b882ffeda9cd18d3cbc3ac8ab3061
Aditya <65461420+Adi142857@users.noreply.github.com>
2022-08-13 23:26:05 +0530

    update posts

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -6,16 +6,18 @@ import Post from './Post/Post';
 import useStyles from './styles';
 
 const Posts = ({ setCurrentId }) => {
-  const posts = useSelector((state) => state.posts);
+  const { posts, isLoading } = useSelector((state) => state.posts);
   const classes = useStyles();
 
+  if (!posts.length && !isLoading) return 'No posts';
+
   return (
-    !posts.length ? <CircularProgress /> : (
+    isLoading ? <CircularProgress /> : (
       <Grid className={classes.container} container alignItems="stretch" spacing={3}>
-        {posts.map((post) => (
-          <Grid key={post._id} item xs={12} sm={6} md={6}>
-            <Post post={post} setCurrentId={setCurrentId} />
-          </Grid>
+        {posts?.map((post) => (
+          <Grid key={post._id} item xs={12} sm={12} md={6} lg={3}>
+          <Post post={post} setCurrentId={setCurrentId} />
+        </Grid>
         ))}
       </Grid>
     )
