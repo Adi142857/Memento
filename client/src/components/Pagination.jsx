fa6e4452ca3ebff367c5455ce70442ec8becf2bf
Aditya <65461420+Adi142857@users.noreply.github.com>
2022-08-13 23:24:31 +0530

    update pagination

--- client/src/components/Pagination.jsx
+++ client/src/components/Pagination.jsx
@@ -1,25 +1,36 @@
-import React from 'react';
-import {Pagination, PaginationItem } from '@material-ui/lab';
-import {Link} from 'react-router-dom';
+/* eslint-disable react/jsx-props-no-spreading */
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Pagination, PaginationItem } from '@material-ui/lab';
+import { Link } from 'react-router-dom';
+
+import { getPosts } from '../actions/posts';
 import useStyles from './styles';
 
-const Paginate=()=>{
-    const classes=useStyles();
+const Paginate = ({ page }) => {
+  const { numberOfPages } = useSelector((state) => state.posts);
+  const dispatch = useDispatch();
 
-    return(
-        <Pagination
-         classes={{ul: classes.ul}}
-         count={5}
-         page={1}
-         variant="outlined"
-         color="primary"
-         renderItem={(item)=>(
-             <PaginationItem { ...item} component={Link} to={`/posts?psge=${1}`}/>
-         )}
-         />
-    );
+  const classes = useStyles();
 
-    
-}
+  useEffect(() => {
+    if (page) {
+      dispatch(getPosts(page));
+    }
+  }, [dispatch, page]);
+
+  return (
+    <Pagination
+      classes={{ ul: classes.ul }}
+      count={numberOfPages}
+      page={Number(page) || 1}
+      variant="outlined"
+      color="primary"
+      renderItem={(item) => (
+        <PaginationItem {...item} component={Link} to={`/posts?page=${item.page}`} />
+      )}
+    />
+  );
+};
 
 export default Paginate;
