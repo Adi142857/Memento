0571b25173a585452c3894ba0e16d4a52db28337
Adi142857 <aditya14iyer@gmail.com>
2022-08-16 17:49:13 +0530

    reducer update

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,19 +1,18 @@
-import { AUTH, LOGOUT } from '../constants/actionTypes';
+import * as actionType from '../constants/actionTypes';
 
+const authReducer = (state = { authData: null }, action) => {
+  switch (action.type) {
+    case actionType.AUTH:
+      localStorage.setItem('profile', JSON.stringify({ ...action?.data }));
 
-const authReducer=(state={authData:null},action)=>{
-    switch(action.type){
-        case AUTH:
-            console.log("inAuthRed",action.payload);
-            localStorage.setItem('profile',JSON.stringify(action.payload.user));
-            localStorage.setItem('token',action.payload.token);
-            return {...state , authData:action?.data};
-        case LOGOUT:
-            localStorage.clear();
-            return { ...state,authData:null};
-        default:
-            return state;
-    }
+      return { ...state, authData: action.data, loading: false, errors: null };
+    case actionType.LOGOUT:
+      localStorage.clear();
+
+      return { ...state, authData: null, loading: false, errors: null };
+    default:
+      return state;
+  }
 };
 
 export default authReducer;
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,19 +1,43 @@
-import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
+import { FETCH_ALL, FETCH_BY_SEARCH, FETCH_BY_CREATOR, FETCH_POST, CREATE, UPDATE, DELETE, LIKE, COMMENT } from '../constants/actionTypes';
 
-export default (posts = [], action) => {
+export default (state = { isLoading: true, posts: [] }, action) => {
   switch (action.type) {
+    case 'START_LOADING':
+      return { ...state, isLoading: true };
+    case 'END_LOADING':
+      return { ...state, isLoading: false };
     case FETCH_ALL:
-      return action.payload;
+      return {
+        ...state,
+        posts: action.payload.data,
+        currentPage: action.payload.currentPage,
+        numberOfPages: action.payload.numberOfPages,
+      };
+    case FETCH_BY_SEARCH:
+    case FETCH_BY_CREATOR:
+      return { ...state, posts: action.payload.data };
+    case FETCH_POST:
+      return { ...state, post: action.payload.post };
     case LIKE:
-      return posts.map((post) => (post._id === action.payload._id ? action.payload : post));
+      return { ...state, posts: state.posts.map((post) => (post._id === action.payload._id ? action.payload : post)) };
+    case COMMENT:
+      return {
+        ...state,
+        posts: state.posts.map((post) => {
+          if (post._id === +action.payload._id) {
+            return action.payload;
+          }
+          return post;
+        }),
+      };
     case CREATE:
-      return [...posts, action.payload];
+      return { ...state, posts: [...state.posts, action.payload] };
     case UPDATE:
-      return posts.map((post) => (post._id === action.payload._id ? action.payload : post));
+      return { ...state, posts: state.posts.map((post) => (post._id === action.payload._id ? action.payload : post)) };
     case DELETE:
-      return posts.filter((post) => post._id !== action.payload);
+      return { ...state, posts: state.posts.filter((post) => post._id !== action.payload) };
     default:
-      return posts;
+      return state;
   }
 };
 
