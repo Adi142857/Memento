58c4aed1dcb75fb45671774e9a6c9dcb484ddd1a
Adi142857 <aditya14iyer@gmail.com>
2022-10-05 22:52:53 +0530

    middleware updated

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,26 +1,28 @@
-import jwt,{decode} from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
-const auth=async(req,res,next)=>{
-    try{
-        const token=req.headers.authorization.split(" ")[1];
-        const isCustomAuth= token.length<500;
-        let decodedData;
+const secret = 'test';
 
-        if(token && isCustomAuth){
-            decodedData=jwt.verify(token, 'test');
+const auth = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const isCustomAuth = token.length < 500;
 
-            req.userId=decodedData?.id;
+    let decodedData;
 
-        }
-        else{
-            decodedData=jwt.decode(token);
-            req.userId=decodedData?.sub;
-        }
+    if (token && isCustomAuth) {      
+      decodedData = jwt.verify(token, secret);
 
-        next();
-    }catch(error){
-        console.log(error);
-    }
-}
+      req.userId = decodedData?.id;
+    } else {
+      decodedData = jwt.decode(token);
+
+      req.userId = decodedData?.sub;
+    }    
+
+    next();
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export default auth;
