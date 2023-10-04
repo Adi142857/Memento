e08128c16e887f4d91c652d35e9eae5d50f1b705
Aditya <65461420+Adi142857@users.noreply.github.com>
2023-10-04 15:07:58 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,19 +8,21 @@ import userRouter from "./routes/user.js";
 
 const app = express();
 
+app.use('/posts', postRoutes);
+app.use("/user", userRouter);
 app.get('/',(req,res)=>{
   res.json("API is running");
 })
 
-app.use('/posts', postRoutes);
-app.use("/user", userRouter);
-
 app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors());
-app.use(cors({
-  origin: '*',
-}));
+
+const corsOptions = {
+  origin: ['http://localhost:3000/', 'https://create-memento.netlify.app/'],
+  credentials: true, // Enable credentials (cookies, headers) for these origins
+};
+
+app.use(cors(corsOptions));
 
 dotenv.config();
 const CONNECTION_URL = process.env.REACT_APP_MONGO_DB_URL;
